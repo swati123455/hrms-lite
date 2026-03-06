347e4d1b59b08a5ad0266a8400e6eebdfb227161
swati <48652139+swati123455@users.noreply.github.com>
2026-03-06 09:17:22 +0530

    fix backend api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-baseURL: "https://hrms-lite-backend.onrender.com/api"
+baseURL: "https://hrms-lite-rk6w.onrender.com/api"
 });
 
 export default api;
